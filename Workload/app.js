7890dce7ea71f0133058a2e480381742d86010b7
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2019-11-06 11:42:27 +0800

    submitable version 1

--- Workload/app.js
+++ Workload/app.js
@@ -6,8 +6,10 @@ var db = monk('localhost:27017/assignment1');
 
 
 
+app.use(express.static('public'));
+
 // allow retrieval of static files and make db accessible to router
-app.use(express.static('public'), function(req,res,next){
+app.use(express.static(__dirname + 'public'), function(req,res,next){
     req.db = db;
     next();
 })
@@ -59,17 +61,47 @@ app.get('/Email',function(req, res){
   });
 })
 
+app.get('/Dummythc',function(req, res){
+  var db = req.db;
+  var collection = db.get('emailList');
+
+
+collection.insert({'sender':'amy@cs.hku.hk','recipient':'john@cs.hku.hk','title':'Meeting today','time':'20:32:01 Fri Oct 11 2019', 'content':'Shall we meet at the library at 5pm?','mailbox':'Inbox'})
+collection.insert({'sender':'professor@cs.hku.hk','recipient':'u3560@hku.hk','title':'Assignment','time':'16:20:00 Thu Oct 10 2019', 'content':'The new assignment has been released','mailbox':'Inbox'})
+collection.insert({'sender':'johnsnow@gmail.com','recipient':'u3560307@hku.hk','title':'My new movie','time':'01:00:00 Mon Oct 7 2019', 'content':'Do you want to watch my new movie with me','mailbox':'Inbox'})
+
+collection.insert({'sender':'jkrowling@fin.hku.hk','recipient':'u3560307@hku.hk','title':'My new Book','time':'15:30:07 Thu Oct 10 2019', 'content':'Do you want to read the new book i wrote?','mailbox':'Important'})
+collection.insert({'sender':'donaldtrump@whitehouse.us','recipient':'u3560307@hku.hk','title':'I quit','time':'00:00:07 Tue Oct 5 2019', 'content':'Im quitting this job!','mailbox':'Important'})
+collection.insert({'sender':'postmaster@nobel.no','recipient':'u3560307@hku.hk','title':'Congratrulations!!','time':' 00:00:00 Sun Nov 3 2019', 'content':'Congratulations on winning the nobel prize!','mailbox':'Important'})
+
+collection.insert({'sender':'u3560307@hku.hk','recipient':'postmaster@nobel.no','title':'Response','time':'00:00:00 Mon Nov 4 2019', 'content':'Thank you!','mailbox':'Sent'})
+collection.insert({'sender':'u3560307@hku.hk','recipient':'donaldtrump@whitehouse.us','title':'Good Luck','time':'00:00:00 Mon Nov 4 2019', 'content':'Good luck at your new job','mailbox':'Sent'})
+collection.insert({'sender':'u3560307@hku.hk','recipient':'barackobama','title':'Yo, hello','time':'00:00:00 Mon Nov 4 2019', 'content':'GGEZ','mailbox':'Sent'})
+
+collection.insert({'sender':'bocsoc@hku.hk','recipient':'u3560307@hku.hk','title':'Scam 1','time':'01:00:00 Mon Nov 4 2019', 'content':'Scammed you 1','mailbox':'Trash'})
+collection.insert({'sender':'bocsoc@hku.hk','recipient':'u3560307@hku.hk','title':'Scam 2','time':'02:00:00 Mon Nov 4 2019', 'content':'Scammed you 2','mailbox':'Trash'})
+collection.insert({'sender':'bocsoc@hku.hk','recipient':'u3560307@hku.hk','title':'Scam 3','time':'03:00:00 Mon Nov 4 2019', 'content':'Scammed you 3','mailbox':'Trash'})
+
+})
+
 app.post('/NewMail', express.urlencoded({ extended: true }), function(req, res){
   var db = req.db;
 	var collection = db.get('emailList');
 
-	var title = req.query.title;
-  var recipient = req.query.recipient;
-  var content = req.query.content;
+	var title = req.body.title;
+  var recipient = req.body.recipient;
+  var content = req.body.content;
+
+  /*console.log(recipient);
+  console.log(title);
+  console.log(content);*/
 
   var d = new Date();
-  var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
 
+  var month = ["Jan","Feb","Mar","April","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+  var day = ["Mon","Tues","Wed","Thu","Fri","Sat","Sun"]
+
+  var date = d.getHours() +":"+ d.getMinutes()+":"+d.getSeconds() + " " + day[d.getDay()] + " " + month[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear();
   var sender = 'u3560307@hku.hk';
 
   collection.insert({'sender':sender,'recipient':recipient,'title':title,'time':date, 'content':content,'mailbox':'Sent'});
---
+++ Workload/javascripts/script.js
@@ -0,0 +1,159 @@
+var count = 0;
+var currentMailBox='';
+
+function countF(){
+  count+=1;
+  LoadInbox(currentMailBox);
+}
+function countB(){
+  count-=1;
+
+  if (count<0){
+    count=0;
+  }
+  LoadInbox(currentMailBox);
+}
+function countClean(){
+  count=0;
+}
+function setBox(box){
+  currentMailBox = box;
+}
+function cleanColour(){
+  document.getElementById("Inbox").style.backgroundColor = "white";
+  document.getElementById("Important").style.backgroundColor = "white";
+  document.getElementById("Sent").style.backgroundColor = "white";
+  document.getElementById("Trash").style.backgroundColor = "white";
+}
+function LoadInbox(poh){
+  setBox(poh);
+  //clears mailbox
+  document.getElementById("email").innerHTML = "";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var holder = JSON.parse(xmlhttp.responseText)
+      //Appends response_string to email division
+      document.getElementById("email").innerHTML = ResHTML(holder);
+      cleanColour();
+      document.getElementById(poh).style.backgroundColor = "#bfbfbf";
+
+    }
+  }
+  xmlhttp.open("GET","inbox?mailBox=" + poh,true);
+  xmlhttp.send();
+}
+function ResHTML(docs) {
+  var response_string = "";
+
+  var number_of_messages = 10;
+
+  var ubound = ((count+1)*number_of_messages);
+  if (ubound>docs.length){
+    ubound = docs.length;
+  }
+  var lbound=(count*number_of_messages);
+  if (lbound>docs.length){
+    lbound = docs.length;
+  }
+
+
+
+  for (var i = lbound; (lbound <= i) && (i < ubound); i++) {
+    var email = docs[i];
+    response_string += "<input type=\"checkbox\" id="+email['_id']+">" + "<div class=\"hope\" onclick=\"LoadEmail("+"\'"+email['_id']+"\'"+");\">";
+    response_string += email['sender'] + " " + email['title'] + " " + email['time'];
+    response_string += "</div></input><br>";
+  }
+  return response_string;
+}
+function MoveHot(mailBox){
+  var Topple = [];
+  var mode = document.getElementById("email").childNodes;
+  var len = mode.length;
+
+  for(var i = 0;i<len;++i){
+    if (mode[i].checked == true){
+      Topple.push(mode[i].id);
+    }
+  }
+
+  for(var i=0;i<Topple.length;++i){
+    Update(Topple[i],mailBox);
+  }
+
+}
+function Update(id,mailBox){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      if(xmlhttp.responseText == 'OK'){
+        LoadInbox(currentMailBox);
+      }
+    }
+  }
+  xmlhttp.open("GET","Update?mailBox=" + mailBox + "&id=" + id,true);
+  xmlhttp.send();
+}
+function LoadEmail(id){
+  //clears mailbox
+  document.getElementById("email").innerHTML = "";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var holder = JSON.parse(xmlhttp.responseText);
+      //Appends response_string to email division
+      document.getElementById("email").innerHTML = ResEmail(holder);
+      cleanColour();
+    }
+  }
+  xmlhttp.open("GET","Email?id=" + id,true);
+  xmlhttp.send();
+}
+function ResEmail(mood){
+  var docs = mood[0];
+  var response_string = "";
+  response_string += "<span style=\"float:left;\">" + docs['title'] + "</span><span style=\"float:right;\">" + docs['time'] + "</span><br>" + docs['sender'] + "<br>" + docs['recipient'] + "<br>";
+  response_string += "<br>" + docs['content'];
+
+  return response_string;
+}
+function LoadCompose(){
+  cleanColour();
+  var response_string="";
+  var email = document.getElementById("email");
+  response_string += "New Message <br>";
+  response_string += "To: <input type=\"text\" style=\"margin-left: 40px;position:relative;\" id=\"recipient\"><br>";
+  response_string += "Subject: <input type=\"text\" style=\"margin-left: 09px;position:relative;\" id=\"title\"><br>";
+  response_string += "<textarea id=\"content\" id=\"content\"></textarea><br><br>";
+  response_string += "<button type=\"button\" onclick=\"PostMan();\">Send</button>";
+  email.innerHTML = response_string;
+}
+function PostMan(){
+  var recipient = document.getElementById("recipient").value;
+  var title = document.getElementById("title").value;
+  var content = document.getElementById("content").value;
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      LoadInbox('Sent');
+    }
+  }
+  xmlhttp.open("POST", "NewMail", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send("recipient="+recipient+"&title="+title+"&content="+content);
+  LoadInbox('Sent');
+}
+function SetDummy(){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      console.log("Ok");
+    }
+  }
+  //Sets the dummy mails;
+  xmlhttp.open("GET","Dummythc",true);
+  xmlhttp.send();
+  LoadInbox('Inbox');
+}
